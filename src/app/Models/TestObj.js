701016c5de50858ab991af47d0bbfca6814b26b2
PreethamOnFire <preethamb2005@gmail.com>
2024-10-05 11:27:29 -0700

    Added List of Test buttons

---
+++ src/app/Models/TestObj.js
@@ -0,0 +1,6 @@
+class TestObj {
+    constructor(name, author) {
+        this.name = name;
+        this.author = author;
+    }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,46 @@
 import styles from "./page.module.css";
 import React from 'react';
 
+class TestObj {
+  constructor(name, author) {
+      this.name = name;
+      this.author = author;
+  }
+}
+
+function TestModuleButtonList({testlist}){
+  const count = testlist.length; 
+  let heading = "";
+  if (count > 0) {
+    const noun = count > 1 ? 'Tests' : 'Test';
+    heading = count + ' ' + noun;
+  }
+  return (
+    <>
+      <h2>{heading}</h2>
+      {testlist.map(test =>
+        <button className={styles.testButton}>
+          <p>{test.name}</p>
+        </button>
+      )}
+    </>
+  );
+}
 
 export default function Home() {
+  let Tests = [];
+  let obj1 = new TestObj("Gay", "Preetham");
+  let obj2 = new TestObj("Gay", "Preetham");
+  let obj3 = new TestObj("Gay", "Preetham");
+  Tests.push(obj1);
+  Tests.push(obj2);
+  Tests.push(obj3);
   return (
     <>
     <div className={styles.MainGrid}>
       <div className={styles.NavBar}>
         <p className={styles.logo}>LOGO</p>
-        <button className={styles.color}>Test 1</button>
+      <TestModuleButtonList className={styles.testButtonList} testlist = {Tests}/>
       </div>
       <div className={styles.content}>
         <header className={styles.title}>K-12 APP</header>
