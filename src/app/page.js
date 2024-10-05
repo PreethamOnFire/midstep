c5e9a42ceee3af35bdcfdb2614f00810be7fcc19
PreethamOnFire <preethamb2005@gmail.com>
2024-10-05 12:32:03 -0700

    tried figureing out some stuff

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@
 import styles from "./page.module.css";
 import React from 'react';
 
+
 class TestObj {
   constructor(name, author) {
       this.name = name;
       this.author = author;
   }
 }
+
+
 
 function TestModuleButtonList({testlist}){
   const count = testlist.length; 
@@ -16,11 +19,13 @@ function TestModuleButtonList({testlist}){
     const noun = count > 1 ? 'Tests' : 'Test';
     heading = count + ' ' + noun;
   }
+  function switchTest(test){
+  }
   return (
     <>
       <h2>{heading}</h2>
       {testlist.map(test =>
-        <button className={styles.testButton}>
+        <button className={styles.testButton} onClick={switchTest(test)}>
           <p>{test.name}</p>
         </button>
       )}
@@ -28,14 +33,28 @@ function TestModuleButtonList({testlist}){
   );
 }
 
+function TestViewer({test}){
+  return(
+    <>
+    <h1>
+      {test.name}
+    </h1>
+    <h2>
+      {test.author}
+    </h2>
+    </>
+  );
+}
+
 export default function Home() {
   let Tests = [];
   let obj1 = new TestObj("Gay", "Preetham");
-  let obj2 = new TestObj("Gay", "Preetham");
-  let obj3 = new TestObj("Gay", "Preetham");
+  let obj2 = new TestObj("gooooo", "Preetham");
+  let obj3 = new TestObj("heheheh", "Preetham");
   Tests.push(obj1);
   Tests.push(obj2);
   Tests.push(obj3);
+  const [content, setContent] = React.useState(<TestViewer test = {obj1}/>);
   return (
     <>
     <div className={styles.MainGrid}>
@@ -45,6 +64,7 @@ export default function Home() {
       </div>
       <div className={styles.content}>
         <header className={styles.title}>K-12 APP</header>
+        {content}
       </div>
     </div>
     </>
