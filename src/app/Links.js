51a5fc4711a9f43affb44f13197946f7fbd8290f
Arjun83-png <arjuntaneja14@gmail.com>
2024-10-06 13:05:50 -0700

    adding js

--- src/app/Links.js
+++ src/app/Links.js
@@ -7,7 +7,13 @@
 // }
 
 export const Tests = [
-  {name: "Straight", author: "Preetham", id: "001"},
-  {name: "gooooo", author: "Preetham", id: "002"},
-  {name: "heheheh", author: "Preetham", id: "003"},
+  {name: "KindergardenIHS.pdf", path: "Backend/Student_Practice/KindergardenIHS.pdf", uthor: "Arjun", id: "001"},
+  {name: "AdditionalQuestions.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_AdditionalQuestions.pdf", author: "Soham", id: "002"},
+  {name: "AnimalFoodChain.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_AnimalFoodChain.pdf", author: "Preetham", id: "003"},
+  {name: "CountingandSummation.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_CountingandSummation.pdf", author: "Vani", id: "004"},
+  {name: "DistanceandTime.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_DistanceandTime.pdf", author: "Vani", id: "005"},
+  {name: "DistributionandDivision.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_DistributionandDivision.pdf",  author: "Soham", id: "006"},
+  {name: "MeasurementandRatio.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_MeasurementandRatio.pdf",  author: "Arjun", id: "007"},
+  {name: "MoneyandBudgeting.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_MoneyandBudgeting.pdf", author: "Preetham", id: "008"},
+  {name: "PercentageandProportions.pdf", path: "Backend/Student_Practice/MathSkillsAssessment_PercentageandProportions.pdf", author: "Preetham", id: "009"}
 ]
